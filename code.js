34ec4e2c22b1c09e81c188198d1903b3669ea4e2
Dev2Mist <benjaminborges.dev@gmail.com>
2025-08-28 22:44:25 -0300

    Cambios estructurales: 1. Se ha modificado la forma en que se mapean los indices del input y del texto

--- code.js
+++ code.js
@@ -1,24 +1,41 @@
+const texto = [
+  "Barton, inquieto, al ver el comentario, le sucede. Soy posible ofrecer con desprecio, señor, distancia, más fuerte. Adjunto, excelencia, anunciando o razonable, soy, si es indulgencia. Exeter habló con un espíritu acordado, no, él no puede. Persianas traicionadas en las cercanías, lo desempacó. En una apariencia tan imposible, consideró al señor. La señora lo dejó, encuentra, son buenos",
+  "Contento, desconfía, certeza, no, son franqueza, jamón oculto. En una resolución no afectada, considerado de. No pensé en mí, esposo o coronel, formando efectos. Fin, sentado, mostrando quién vio, además, hijo, musical adaptado. Contrastado, interesado, comer, alteración, pianoforte, simpatizar, fue. Él, familias, creyó, si no, elegancia, interés, sorpresa. Se quedó, millas equivocadas, un plato de retraso. Ella, relación propia, puesta, sobrevivida, puede dispuesta.",
+  "Sexo y principio descuidado, pregunta, entusiasta, consultado. Objeto, comentario, animado, todo, débilmente, disculpó nuestro arbolado. Viejo, su objeto, hablador, respeto, vulgar, perdido. Hablando, lanzando, crianza, traicionó, niños, mi a. Yo, Marianne, no, él, horrible, produjo, vosotros. Suficientemente desagradable, un insensible inmóvil si se introduce. Ahora, ni ambos se acerquen demasiado tarde.",
+];
+
+const usuarioInput = document.querySelector("#user-input");
+const text = document.querySelector("#text");
 let currentIndex = -1;
 
-function updateText(inputItemId, targetItemId) {
-  const userInput = document.getElementById(inputItemId);
-  const target = document.getElementById(targetItemId);
-  const test = document.getElementById("input-clone");
-  let charCopy;
+let textoSeleccionado = (text.textContent = textSelection(texto));
 
-  userInput.oninput = function () {
-    currentIndex++;
+usuarioInput.addEventListener("input", (e) => {
+  console.clear();
+  console.log("Current index: " + currentIndex);
+  let followingIndex = currentIndex;
 
-    charCopy = userInput.value.charAt(currentIndex);
+  if (e.inputType == "deleteContentBackward" && currentIndex >= 0)
+    currentIndex--;
+  else currentIndex++;
 
-    if (charCopy == target.textContent[currentIndex]) {
-      test.textContent += charCopy;
-    } else {
-      console.log(
-        `La letra que escribiste es ${charCopy} y deberias haber escrito ${target.textContent[currentIndex]}`
-      );
-    }
-  };
+  if (usuarioInput.value[currentIndex] != text.textContent[currentIndex]) {
+    followingIndex = currentIndex;
+    console.log(`Indice en el que se cometió un error ${followingIndex}`);
+  }
+  console.log(
+    `Input del usuario: ${usuarioInput.value[currentIndex]}\nTexto en el indice actual: ${text.textContent[currentIndex]}`
+  );
+  console.log(
+    "Resultado de la comparacion: ",
+    usuarioInput.value[currentIndex] == text.textContent[currentIndex]
+  );
+});
+
+function textSelection(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-updateText("user-input", "target-text");
+// TODO -> 1. Cuando el usuario comete un error, se puede indicar el indice en el cuál se cometió el  error, para corregirlo, el usuario debe borrar hasta dicho indice y escribir la letra que corresponda.
+// TODO -> 2. Hacer que el color del texto se modifique cuando
+// TODO -> 3. Al borrar con "Ctrl + Delete" ocurre un problema ya que el indice se modifica en 1. Las solución quizá es utilizar alguna funcion que mapee correctamente el indice del string en el cual se encuentra el usuario.
